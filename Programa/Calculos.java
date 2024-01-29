5d277a3aa0ea6b20f7cf0662769b8a94f3b31c2f
Jefferson Queiroz <58812763+jeffersonqpm@users.noreply.github.com>
2024-01-29 19:10:50 -0300

    Criação getMedia e o metodo calcMedia

--- Programa/Calculos.java
+++ Programa/Calculos.java
@@ -1,5 +1,17 @@
 package Programa;
 
 public class Calculos {
+
+    private float media;
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void calcMedia(float soma, int quantidadeNotas){
+
+        media = soma / quantidadeNotas;
+
+    }
     
 }
